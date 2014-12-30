762e1ba0d3bcd307f760b47219de471a595e7213
Pocket <p0ck31@gmail.com>
2014-12-30 12:35:01 -0800

    Add upvotes and make sure it still works

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ function($scope){
 {title: 'post 4', upvotes: 9},
 {title: 'post 5', upvotes: 4}
 ];
-
 // figure out why this goes within $scope as oposed to outside this function
 $scope.addPost = function(){
   //don't let them make an empty post
@@ -19,6 +18,10 @@ $scope.addPost = function(){
   //we take the post text from $scope.title, which we then clear
   $scope.posts.push({title: $scope.title, upvotes: 0});
   $scope.title = '';
+};
+
+$scope.incrementUpvotes = function(post) {
+  post.upvotes += 1;
 };
 
 }]);
