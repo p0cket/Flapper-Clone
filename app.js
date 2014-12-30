6f0bd7a29a3f059dba6d2424e27739b34327c657
Pocket <p0ck31@gmail.com>
2014-12-30 12:49:18 -0800

    Add links and linkable postings

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ angular.module('flapperNews', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
 
   $scope.posts = [
 {title: 'post 1', upvotes: 5},
@@ -12,16 +11,23 @@ function($scope){
 {title: 'post 5', upvotes: 4}
 ];
 // figure out why this goes within $scope as oposed to outside this function
+
 $scope.addPost = function(){
   //don't let them make an empty post
   if(!$scope.title || $scope.title === '') { return; }
-  //we take the post text from $scope.title, which we then clear
-  $scope.posts.push({title: $scope.title, upvotes: 0});
+  $scope.posts.push({
+    title: $scope.title,
+    link: $scope.link,
+    upvotes: 0
+  });
+  //we take the post text from $scope.title and $scope.link, which we then clear
   $scope.title = '';
+  $scope.link = '';
 };
 
 $scope.incrementUpvotes = function(post) {
   post.upvotes += 1;
 };
+
 
 }]);
