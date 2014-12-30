c80171b8176b124f2af843ea40c1ab8f46dcc115
Pocket <p0ck31@gmail.com>
2014-12-30 12:26:02 -0800

    Commit baby steps so you can revert

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ function($scope){
 {title: 'post 4', upvotes: 9},
 {title: 'post 5', upvotes: 4}
 ];
+
+// figure out why this goes within $scope as oposed to outside this function
 $scope.addPost = function(){
-  $scope.posts.push({title: 'A new post!', upvotes: 0});
+  //don't let them make an empty post
+  if(!$scope.title || $scope.title === '') { return; }
+  //we take the post text from $scope.title, which we then clear
+  $scope.posts.push({title: $scope.title, upvotes: 0});
+  $scope.title = '';
 };
+
 }]);
