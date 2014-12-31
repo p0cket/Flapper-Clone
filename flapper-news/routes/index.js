e60c26a225b593b3f0d98c8f762968f363a26935
Pocket <p0ck31@gmail.com>
2014-12-30 17:57:54 -0800

    Create -> Creating Our First Route

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -7,3 +7,15 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
+
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
